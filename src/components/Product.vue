483ab8dc71e6e30265fc964d04d27482fe5fb08f
Wade Austin <wade@few.io>
2021-02-04 14:51:09 -0600

    tweak product display

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,49 +1,66 @@
 <template>
-    <div class="product">
+    <div class="product-component">
+        <div class="loading" v-if="loading">Loading&hellip;</div>
+        <div class="product" v-else>
+            <header class="product-header columns mb-3">  
+                
+                <div v-if="selectedImage" class="column is-two-fifths selected-image-col">
+                    <img class="selected-image" :src="selectedImage.src" :key="selectedImage.id"/>
+                </div>  
 
-        <header class="product-header columns mb-3">  
-            
-            <div v-if="selectedImage" class="column is-two-fifths selected-image-col">
-                <img class="selected-image" :src="selectedImage.src" :key="selectedImage.id"/>
-            </div>  
+                <div class="column info">
+                    <h2 class="is-size-2">{{product.title}}</h2>
+                    <p class="description mb-2" v-if="product.description">
+                        {{product.description}}
+                    </p>
+                    <p>
+                        <strong>Product Available For Sale: {{product.availableForSale}}</strong>
+                    </p>
 
-            <div class="column info">
-                <h2 class="is-size-2">{{product.title}}</h2>
-                <p class="description mb-2" v-if="product.description">
-                    {{product.description}}
-                </p>
-                <p>
-                    <strong>Product Available For Sale: {{product.availableForSale}}</strong>
-                </p>
-            </div>
-        </header>
-
-
-        <div class="image-gallery">
-            <img v-for="image in product.images" :key="image.id" :src="image.src"
-                @click="onGalleryImageClick(image)" />
-        </div>
-
-            <div class="variant-wrapper">
-                <div class="variant" v-for="v in product.variants" :key="v.id" >
-                    title: {{v.title}}<br>
-                    price: {{v.price}}<br>
-                    available: {{v.available}}<br>
-                    <img v-if="v.image" :src="v.image.src" />
+                    <div class="product-options columns">
+                        <div v-for="opt in product.options" :key="opt.id">
+                            <div class="field column">
+                                <label class="label">{{opt.name}}</label>
+                                <div class="select">
+                                    <select change="onOptionChange(opt, $event)">
+                                        <option v-for="(value, idx) in opt.values" :key="idx">
+                                            {{value}}
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-    
+            </header>
+
+
+            <div class="image-gallery">
+                <img v-for="image in product.images" :key="image.id" :src="image.src"
+                    @click="onGalleryImageClick(image)" />
             </div>
 
-        <h4 class="is-size-4">Product Data</h4>
-         <p>
-            ID: {{id}}<br/>
-            ENCODED PRODUCT ID: {{encodeShopifyProductId(id)}}
+                <div class="variant-wrapper">
+                    <div class="variant" v-for="v in product.variants" :key="v.id" >
+                        title: {{v.title}}<br>
+                        price: {{v.price}}<br>
+                        available: {{v.available}}<br>
+                        <img v-if="v.image" :src="v.image.src" />
+                    </div>
         
-        </p>
-        <pre>
-            {{ product }}
-        </pre>
-        
+                </div>
+
+            <h4 class="is-size-4">Product Data</h4>
+            <p>
+                ID: {{id}}<br/>
+                ENCODED PRODUCT ID: {{encodeShopifyProductId(id)}}
+            
+            </p>
+            <pre>
+                {{ product }}
+            </pre>
+            
+        </div>
     </div>
 </template>
 
@@ -67,11 +84,13 @@ export default {
             shopify_store_domain: process.env.VUE_APP_SHOPIFY_STORE_DOMAIN,
             shopify_storefront_access_token: process.env.VUE_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
             shopifyClient: null,
+            loading: false,
             product: null,
             selectedImage: null,
         }
     },
     created() {
+        this.loading = true;
         this.shopifyClient = ShopifyClient.buildClient({
             domain: this.shopify_store_domain,
             storefrontAccessToken: this.shopify_storefront_access_token
@@ -84,7 +103,7 @@ export default {
             const productID = this.encodeShopifyProductId(this.id);
             try  {
                 this.product = await this.shopifyClient.product.fetch(productID);
-
+                this.loading = false;
                 if(this.product.images.length > 0) {
                     this.selectedImage = this.product.images[0];
                 }
@@ -104,6 +123,11 @@ export default {
         },
         onGalleryImageClick(image) {
             this.selectedImage = image;
+        },
+        onOptionChange(opt, event) {
+            console.log('onOptionChange');
+            console.log(opt);
+            console.log(event);
         }
     },
     filters: {
