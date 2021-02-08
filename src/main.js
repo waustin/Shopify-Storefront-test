b7ae89723c01e6061f13baedeb2e671b32273509
Wade Austin <wade@few.io>
2021-02-08 13:04:18 -0600

    get a boost query working with shopify

--- src/main.js
+++ src/main.js
@@ -4,9 +4,29 @@ import App from './App.vue'
 import './assets/sass/main.scss';
 import router from './router'
 
+
+
+
+import ApolloClient from 'apollo-boost';
+const SHOPIFY_GQL_URI = 'https://' + process.env.VUE_APP_SHOPIFY_STORE_DOMAIN + '//api/2021-01/graphql.json';
+const apolloClient = new ApolloClient({
+    uri: SHOPIFY_GQL_URI,
+    headers: {
+      'X-Shopify-Storefront-Access-Token': process.env.VUE_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN
+    }
+});
+
+import VueApollo from 'vue-apollo'
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+});
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
--- src/views/ShopifyApollo.vue
+++ src/views/ShopifyApollo.vue
@@ -1,12 +1,104 @@
 <template>
   <div>
       <h1 class="title">Shopify Apollo Page</h1>
+      <pre>
+          Shop Name: {{shop.name}}<br>
+          Shop Description: {{shop.description}}
+          Shop Primary Domain: {{shop.primaryDomain}}
+          Product: {{shop.products}}
+      </pre>
   </div>
 </template>
 
 <script>
+
+import gql from 'graphql-tag'
+
 export default {
     name: 'SpofiyApollo',
+    apollo: {
+        shop: gql`{
+            shop { 
+                name
+                description
+                primaryDomain {
+                    host
+                    url
+                }
+                products(first: 10) {
+                    edges {
+                        node {
+                            id
+                            title
+                            handle
+                            availableForSale
+                            priceRange {
+                                maxVariantPrice {
+                                    amount
+                                    currencyCode
+                                }
+                                minVariantPrice {
+                                    amount
+                                    currencyCode
+                                }
+                            }
+                            options {
+                                name
+                                values
+                            }
+                            images(first: 10) {
+                                edges {
+                                    node {
+                                        id
+                                        altText
+                                        originalSrc
+                                        transformedSrc
+                                        height
+                                        width
+                                    }
+                                }
+                            }
+                            variants(first: 20) {
+                                edges {
+                                    node {
+                                        id
+                                        title
+                                        availableForSale
+                                        currentlyNotInStock
+                                        priceV2 {
+                                            amount
+                                            currencyCode
+                                        }
+                                        image {
+                                            id
+                                            originalSrc
+                                            height
+                                            width
+                                        }
+                                        selectedOptions {
+                                           name
+                                           value 
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            } 
+        }`,
+    },
+    data() {
+        return {
+            shop: '',
+        };
+    },
+    created() {
+        console.log('Shopify Apollo Created');
+        console.log(this.$apollo.provider.clients);
+
+    }
 }
 </script>
 
